adf65973bb0a0154210ffe701cb6691aa3712bb6
AzusaChino <azusa146@gmail.com>
2021-05-21 17:54:15 +0800

    update cors config

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"iris/router"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"iris/router"
 )
 
 func main() {
 	app := gin.Default()
 
-	app.Use(cors.Default())
+	cors := cors.New(cors.Config{
+		//准许跨域请求网站,多个使用,分开,限制使用*
+		AllowOrigins: []string{"*"},
+		//准许使用的请求方式
+		AllowMethods: []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		//准许使用的请求表头
+		AllowHeaders: []string{"Origin", "Authorization", "Content-Type"},
+		//显示的请求表头
+		ExposeHeaders: []string{"Content-Type"},
+		//凭证共享,确定共享
+		AllowCredentials: true,
+		//容许跨域的原点网站,可以直接return true允许所有
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		//超时时间设定
+		MaxAge: 1 * time.Hour,
+	})
+
+	app.Use(cors)
 
 	router.ApplyRouter(app)
 
